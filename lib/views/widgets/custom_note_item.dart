90829d08e75abbf6a2cc50ce92b9b5c57ed8217f
Anas <ahmdbwzy14@gmail.com>
2025-01-22 14:53:04 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,56 +1,81 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 20, top: 20, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Notes App",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
+    return GestureDetector(
+      onLongPress: () {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.info,
+          animType: AnimType.bottomSlide,
+          title: 'Edit Note',
+          desc: 'Are you sure you want to',
+          btnCancelOnPress: () {},
+          btnOkOnPress: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const EditNoteView();
+                },
               ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              child: Text(
-                "I am anas bouzi flutter mobile app developer",
+            );
+          },
+        ).show();
+      },
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 20, top: 20, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Notes App",
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 16,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Text(
+                  "I am anas bouzi flutter mobile app developer",
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.5),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black.withOpacity(.4),
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                size: 24,
-                color: Colors.black.withOpacity(.4),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "9 January 2025",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 16),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "9 January 2025",
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
