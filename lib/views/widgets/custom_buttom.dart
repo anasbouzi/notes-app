b32fe312c54a64999f12506827f495efe576c9a0
Anas <ahmdbwzy14@gmail.com>
2025-02-01 18:47:28 +0300

    take input and validate data

--- lib/views/widgets/custom_buttom.dart
+++ lib/views/widgets/custom_buttom.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:notes/constant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 48,
-      decoration: BoxDecoration(
-        color: const Color(kprimaryColorValue),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-              fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 48,
+        decoration: BoxDecoration(
+          color: const Color(kprimaryColorValue),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,17 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:notes/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField(
-      {super.key,
-      this.maxLines = 1,
-      required this.hintText,
-      this.textInputAction});
+  const CustomTextField({
+    super.key,
+    this.maxLines = 1,
+    required this.hintText,
+    this.textInputAction,
+    this.onSaved,
+  });
   final String hintText;
   final TextInputAction? textInputAction;
   final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'الحقل مطلوب';
+        } else {
+          return null;
+        }
+      },
       cursorColor: const Color(kprimaryColorValue),
       maxLines: maxLines,
       textInputAction: textInputAction,
